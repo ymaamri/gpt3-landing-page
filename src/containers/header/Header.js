f5a0a627feb5f8540ecca26f811c62e7769eb048
Yassine Maamri <ymvtbusiness@gmail.com>
2023-06-18 17:24:09 +0100

    new update

---
+++ src/containers/header/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./header.css";
+import ai from "../../assets/ai.png";
+
+function Header() {
+  return (
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let's Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your Email Adress" />
+          <button type="button">Get Started</button>
+        </div>
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} alt="ai" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
